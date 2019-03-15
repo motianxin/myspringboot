4049ad51a4759d3315ced8a7c49a3bfc159a371e
zhigang <2812058711@qq.com>
2019-03-16 01:02:30 +0800

    just test

---
+++ myspringboot/src/test/java/com/zhigang/myspringboot/service/AlarmSyncServiceTest.java
@@ -0,0 +1,44 @@
+package com.zhigang.myspringboot.service;
+
+import com.zhigang.myspringboot.MyspringbootApplicationTests;
+import com.zhigang.myspringboot.domain.AlarmSync;
+import com.zhigang.myspringboot.domain.alarmws.NotifyAlarmSyncReq;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AlarmSyncServiceTest extends MyspringbootApplicationTests {
+
+	@Autowired
+	private SyncService syncService;
+
+	@Test
+	public void saveAlarmSync() {
+		NotifyAlarmSyncReq req = new NotifyAlarmSyncReq(47, 88, "hb", "yueme");
+
+		try {
+			syncService.saveAlarmSync(req);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void getOneById() {
+		AlarmSync alarmSync = syncService.getOneById(1l);
+		System.out.println(alarmSync);
+	}
+
+	@Test
+	public void deleteAlarmSync() {
+		syncService.deleteAlarmSync(1l);
+		syncService.getAllAlarmSync().forEach(System.out :: println);
+	}
+
+	@Test
+	public void getAllAlarmSync() {
+		List<AlarmSync> alarmSyncList = syncService.getAllAlarmSync();
+		alarmSyncList.forEach(System.out :: println);
+	}
+}
