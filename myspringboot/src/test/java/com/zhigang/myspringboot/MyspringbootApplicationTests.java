6a3b72865932812c0eac15d3817c5868369aaa04
zghuang <zghuang@fiberhome.com>
2019-03-15 15:29:16 +0800

    node

--- myspringboot/src/test/java/com/zhigang/myspringboot/MyspringbootApplicationTests.java
+++ myspringboot/src/test/java/com/zhigang/myspringboot/MyspringbootApplicationTests.java
@@ -1,16 +1,25 @@
 package com.zhigang.myspringboot;
 
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class MyspringbootApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Before
+	public void init() {
+		System.out.println("开始测试-----------------");
+	}
+
+	@After
+	public void after() {
+		System.out.println("测试结束-----------------");
 	}
 
 }
