9fcdba7b205e813605557627253faaaa6441ecb9
zghuang <zghuang@fiberhome.com>
2019-04-26 11:45:25 +0800

    添加druid

---
+++ myspringboot/src/main/java/com/zhigang/myspringboot/configuration/druid/DruidConfig.java
@@ -0,0 +1,36 @@
+/**
+ * FileName: DruidConfig
+ * Author:   zghuang
+ * Date:     2019/4/26 11:17
+ * Description: DruidConfig
+ * History:
+ * <author>          <time>          <version>          <desc>
+ */
+package com.zhigang.myspringboot.configuration.druid;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 〈DruidConfig〉
+ *
+ * @author zghuang
+ * @create 2019/4/26 11:17
+ * @version 3.2.2
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/");
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        servletRegistrationBean.addInitParameter("deny","192.168.0.19");
+        servletRegistrationBean.addInitParameter("loginUsername","druid");
+        servletRegistrationBean.addInitParameter("loginPassword","druid123!");
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+}
