189d223b07391f0a4a43ea5ccfdd6c0082f710e1
zghuang <zghuang@fiberhome.com>
2019-04-28 17:29:49 +0800

    自定义IP校验器

---
+++ myspringboot/src/main/java/com/zhigang/myspringboot/utils/validator/IPValidator.java
@@ -0,0 +1,38 @@
+/**
+ * FileName: IPValidator
+ * Author:   zghuang
+ * Date:     2019/4/28 17:05
+ * Description: 自定义数据校验器
+ * History:
+ * <author>          <time>          <version>          <desc>
+ */
+package com.zhigang.myspringboot.utils.validator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 〈自定义数据校验器〉
+ *
+ * @author zghuang
+ * @create 2019/4/28 17:05
+ * @version 3.2.2
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Constraint(validatedBy = IPValidatorClass.class)
+public @interface IPValidator {
+    /**
+     * IP 类型：IPV4或者IPV6
+     * @return
+     */
+    String type() default "IPV4";
+
+    String message() default "the ip is illegitimate";
+
+}
---
+++ myspringboot/src/main/java/com/zhigang/myspringboot/utils/validator/IPValidatorClass.java
@@ -0,0 +1,57 @@
+/**
+ * FileName: IPValidatorClass
+ * Author:   zghuang
+ * Date:     2019/4/28 17:08
+ * Description: IP字符串校验器
+ * History:
+ * <author>          <time>          <version>          <desc>
+ */
+package com.zhigang.myspringboot.utils.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 〈IP字符串校验器〉
+ *
+ * @author zghuang
+ * @create 2019/4/28 17:08
+ * @version 3.2.2
+ */
+public class IPValidatorClass implements ConstraintValidator<IPValidator, Object> {
+
+    /**
+     * 定义IPV4正则表达式
+     */
+    private static final String IPV4_REGEX = "^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|[1-9])\\." +
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\." +
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\." +
+            "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)$";
+
+    /**
+     * 定义IPV6正则表达式
+     */
+    private static final String IPV6_REGEX = "^\\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:)))(%.+)?\\s*$";
+
+    private String type;
+
+    @Override
+    public void initialize(IPValidator constraintAnnotation) {
+        this.type = constraintAnnotation.type();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        boolean flag = true;
+        if (value == null) {
+            return flag;
+        }
+
+        if (type.equalsIgnoreCase("ipv4")) {
+            flag = type.matches(IPV4_REGEX);
+        } else if (type.equalsIgnoreCase("ipv6")) {
+            flag = type.matches(IPV6_REGEX);
+        }
+        return flag;
+    }
+}
